0ff91aa28dff59b2c722ced6c3e4c30aed89a157
Maxson Almeida <maxsonferovante@gmail.com>
2025-02-26 20:56:38 -0300

    Adiciona serviço S3 com função para gerar URL pré-assinada

---
+++ app/services/s3_service.py
@@ -0,0 +1,27 @@
+from app.services.boto_aws import ServiceNameAWS, get_instance_aws
+from app.core.config import config, get_logger
+
+logger = get_logger(__name__)
+
+class S3Service:
+    def __init__(self):
+        self.aws = get_instance_aws(
+            ServiceNameAWS.S3
+        )
+        self.bucket_name = config.BUCKET_NAME
+
+    # gerar um link pre-assinado
+    def generate_presigned_url(self, object_name: str, expiration: int = 180000) -> str:
+        try:
+            logger.info(f'Generating presigned URL for object {object_name}')
+            response = self.aws.generate_presigned_url(
+                'get_object',
+                Params={'Bucket': self.bucket_name, 'Key': object_name},
+                ExpiresIn=expiration,                
+            )
+            logger.info(f'Presigned URL generated for object {object_name}')
+            return response
+        except Exception as e:
+            logger.error(f'Error generating presigned URL for object {object_name}')
+            raise e
+    
